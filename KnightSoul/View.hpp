da75cb320781c0f4a74a1f9bd528412c087a0669
Knight <790184126@qq.com>
2017-03-23 14:22:25 +0800

    add view class

---
+++ KnightSoul/View.hpp
@@ -0,0 +1,19 @@
+//
+//  View.hpp
+//  KnightSoul
+//
+//  Created by Knight on 2017/3/23.
+//  Copyright © 2017年 Knight. All rights reserved.
+//
+
+#ifndef View_hpp
+#define View_hpp
+
+#include <stdio.h>
+
+class View
+{
+    
+};
+
+#endif /* View_hpp */
